5ea26a37cfa27ed130e5a0d7e0200ecd81848835
Guru Bhavishya <gurubhavishya33@gmail.com>
2026-07-30 19:34:29 +0530

    Added Grade Calculator program

---
+++ 03_grade_calculator.py
@@ -0,0 +1,10 @@
+n=int(input("Enter your marks:"))
+marks=0
+if 90>=marks<100:
+    print("Grade A+")
+elif 80>=marks<90:
+    print("Grade A")
+elif 70>=marks<80:
+    print("Grade B")
+else:
+    print("Grade C")
