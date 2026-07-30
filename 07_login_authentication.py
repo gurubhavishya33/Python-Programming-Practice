5597756b2fde15fbdd82a6c08fdaa652b46af63a
Guru Bhavishya <gurubhavishya33@gmail.com>
2026-07-30 19:38:07 +0530

    Added Login Authentication program

---
+++ 07_login_authentication.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "npython123":
+    print("Login Successful")
+else:
+    print("Invalid Username or Password")
